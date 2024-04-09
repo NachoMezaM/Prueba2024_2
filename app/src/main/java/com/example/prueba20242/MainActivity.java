8a9321ea0b67205c6830bd9502fbb02b572b0054
NachoMezaM <nachom1605@gmail.com>
2024-04-09 17:25:13 -0400

    Ola

--- app/src/main/java/com/example/prueba20242/MainActivity.java
+++ app/src/main/java/com/example/prueba20242/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.prueba20242;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txt;
+    private Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txt = findViewById(R.id.texto);
+        btn1 = findViewById(R.id.boton1);
+        btn2 = findViewById(R.id.boton2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Hola");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Mundo");
+            }
         });
     }
 }
